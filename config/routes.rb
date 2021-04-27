749f7481fae7be6bc588dc1081e0c4ad823fbb5b
Manuel  Montalvo <manny@Manuels-MacBook-Pro.local>
2021-04-26 21:00:53 -0400

    auth complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   post '/auth/login', to: 'authentication#login'
-  
+  get '/auth/verify', to: 'authentication#verify'
   
   resources :likes, :except => [:update]
   resources :comments
