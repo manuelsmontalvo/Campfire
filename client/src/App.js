ee0fd39a10a943236f123de284c18f693fffc81d
Manuel  Montalvo <manny@Manuels-MacBook-Pro.local>
2021-04-27 18:02:45 -0400

    auth.js, api-helper, folder structure, assets

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { useState, useEffect } from 'react';
+import { Switch, Route, useHistory } from 'react-router-dom'
+
+function App() {
+  
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default App;
