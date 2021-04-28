86a5b5b887f49589bf690448810fe6590f2a8f88
Manuel  Montalvo <manny@Manuels-MacBook-Pro.local>
2021-04-28 16:48:24 -0400

    layout.jsx done

---
+++ client/src/layout/Layout.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function Layout(props) {
+  const { currentUser, handleLogout } = props;
+
+  return (
+    <div>
+      <div className = "nav">
+      <p className="title">Campfire</p>
+      <link>Profile</link>
+      <link>Campsites</link>
+      {
+          currentUser ?
+            <>
+              <button className = "logout_btn" onClick={handleLogout}>Logout</button>
+            </>
+            :
+            <Link className = "login_link" to='/login'>Login/Register</Link>
+        }
+      </div>
+      {props.children}
+    </div>
+  )
+}
