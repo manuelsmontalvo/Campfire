f7a1ea513fdd3d210fa29b170ca92b4d6ea1e951
Manuel  Montalvo <manny@Manuels-MacBook-Pro.local>
2021-04-30 12:45:35 -0400

    error handled

--- client/src/container/MainContainer.jsx
+++ client/src/container/MainContainer.jsx
@@ -8,7 +8,6 @@ import PostDetail from "../screens/PostDetail";
 import Profile from "../screens/Profile";
 
 import { getAllBlogs, createBlog, deleteblog } from "../services/blogs";
-import { getPost, deletePost, updatePost } from "../services/posts";
 
 
 export default function MainContainer({ currentUser }) {
